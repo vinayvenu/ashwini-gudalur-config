4339918990bb67763a38795ff82ebb32b6ea1d0d
sadiag <sadiag@INsadiag.local>
2016-03-14 18:00:28 +0530

    sadia | correction in custom sql

--- openmrs/apps/reports/sql/admissionvisitcount.sql
+++ openmrs/apps/reports/sql/admissionvisitcount.sql
@@ -6,7 +6,7 @@ Select (select count(*) from patient p
   join obs o on o.encounter_id=e.encounter_id and o.value_numeric is NOT NULL or o.value_coded is not null or o.value_text is not null
   join orders od on o.order_id=od.order_id and order_type_id=3
   JOIN concept c on c.concept_id = o.concept_id
-  join concept_class cc on cc.concept_class_id = c.class_id and cc.concept_class_id=26 where o.date_created between between '#startDate#' and '#endDate#') as TribalIPD,
+  join concept_class cc on cc.concept_class_id = c.class_id and cc.concept_class_id=26 where o.date_created  between '#startDate#' and '#endDate#') as TribalIPD,
   (select count(*) from patient p
   join person_attribute pa on p.patient_id=pa.person_id and pa.person_attribute_type_id=27 AND pa.value=1
   join visit v on p.patient_id = v.patient_id
--- openmrs/apps/reports/sql/patientwisediagnosis.sql
+++ openmrs/apps/reports/sql/patientwisediagnosis.sql
@@ -1,8 +1,8 @@
-select p.person_id,CONCAT(pn.given_name, " ", pn.family_name) as Name,padd.city_village,v.visit_id,e.encounter_id,GROUP_CONCAT(DISTINCT(cdvn.name)) from  confirmed_diagnosis_view_new cdvn
+select CONCAT(pn.given_name, " ", pn.family_name) as Name,p.gender,padd.city_village,GROUP_CONCAT(DISTINCT(cdvn.name)) as Diagnosis from  confirmed_diagnosis_view_new cdvn
   left join visit v on cdvn.visit_id=v.visit_id
   left join encounter e on v.visit_id = e.visit_id
   left join person p on cdvn.person_id=p.person_id
   left join person_name pn on p.person_id = pn.person_id
   left join person_address padd on p.person_id = padd.person_id
-  where v.date_created is between '#startDate#' and '#endDate#'
+  where v.date_created  between '#startDate#' and '#endDate#'
 group by cdvn.visit_id;
