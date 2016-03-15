9dceca1ec2b0aac9e3f6596812bdaa89dd2ab21e
sadiag <sadiag@INsadiag.local>
2016-03-15 12:32:34 +0530

    sadia | added custom sql report

--- openmrs/apps/reports/sql/admissionvisitcount.sql
+++ openmrs/apps/reports/sql/admissionvisitcount.sql
@@ -21,7 +21,7 @@ where cast(v.date_created as DATE) between '#startDate#' and '#endDate#' ORDER B
          join visit v on p.patient_id = v.patient_id
          join visit_type vt on vt.visit_type_id = v.visit_type_id and vt.visit_type_id=(select visit_type_id from visit_type where name='Special OPD')
          join encounter e on e.visit_id=v.visit_id
-       where cast(v.date_created as DATE) between '2016-03-15' and '2016-03-15' ORDER BY v.visit_id) as TribalSplOPD,
+       where cast(v.date_created as DATE) between '#startDate#' and '#endDate#' ORDER BY v.visit_id) as TribalSplOPD,
        (select count(person_id) from patient p
          join person_attribute pa on p.patient_id=pa.person_id and pa.person_attribute_type_id=27 AND pa.value=1
          join visit v on p.patient_id = v.patient_id
@@ -37,13 +37,13 @@ where cast(v.date_created as DATE) between '#startDate#' and '#endDate#' ORDER B
        (select count(person_id) from patient p
          join person_attribute pa on p.patient_id=pa.person_id and pa.person_attribute_type_id=27 AND pa.value=2
          join visit v on p.patient_id = v.patient_id
-         join visit_type vt on vt.visit_type_id = v.visit_type_id and vt.visit_type_id=3
+       join visit_type vt on vt.visit_type_id = v.visit_type_id and vt.visit_type_id=(select visit_type_id from visit_type where name='IPD')
          join encounter e on e.visit_id=v.visit_id
        where cast(v.date_created as DATE) between '#startDate#' and '#endDate#' ORDER BY v.visit_id) as NonTribalIPD,
        (select count(person_id) from patient p
          join person_attribute pa on p.patient_id=pa.person_id and pa.person_attribute_type_id=27 AND pa.value=2
          join visit v on p.patient_id = v.patient_id
-         join visit_type vt on vt.visit_type_id = v.visit_type_id and vt.visit_type_id=4 or vt.visit_type_id=9
+         join visit_type vt on vt.visit_type_id = v.visit_type_id and vt.visit_type_id=(select visit_type_id from visit_type where name='OPD')
          join encounter e on e.visit_id=v.visit_id
        where cast(v.date_created as DATE)  between '#startDate#' and '#endDate#' ORDER BY v.visit_id) as NonTribalOPD,
        (select count(person_id) from patient p
