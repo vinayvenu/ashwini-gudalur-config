39c6e43c1d79087d1b813e2290257b4eb86ef8c8
sandye <sandeepelambassery@gmail.com>
2016-03-15 18:58:59 +0530

    add visiting attribute

--- openerp/bahmni_custom/attribute_in_sale_order.py
+++ openerp/bahmni_custom/attribute_in_sale_order.py
@@ -61,6 +61,20 @@ class sale_order(osv.osv):
             else:
                 res[sale_order.id]=""
         return res
+
+    def _get_partner_attribute_Visiting(self, cr, uid, ids, name, args, context=None):
+        res = {}
+        for sale_order in self.browse(cr, uid, ids):
+            partner_obj = self.pool.get("res.partner")
+            partner = partner_obj.browse(cr,uid,sale_order.partner_id.id)
+            partner_attri_cnt=self.pool.get("res.partner.attributes").search(cr,uid,[('partner_id','=',partner.id)])
+            if len(partner_attri_cnt) > 0:
+                partner_attribute=self.pool.get("res.partner.attributes").browse(cr,uid,partner_attri_cnt[0])
+                res[sale_order.id] = partner_attribute.x_Visiting
+            else:
+                res[sale_order.id]=""
+        return res
+
     def onchange_partner_id(self, cr, uid,ids, partner_id, context = None):
         res = super(sale_order, self).onchange_partner_id(cr, uid, ids,partner_id,context=context)
         if partner_id:
@@ -71,17 +85,20 @@ class sale_order(osv.osv):
                 res['value']['partner_is_tribe'] = partner_attribute.x_Is_Tribal
                 res['value']['partner_is_sangam'] = partner_attribute.x_Is_Sangam
                 res['value']['partner_is_Premium'] = partner_attribute.x_Is_Premium_Paid
+                res['value']['partner_visting'] = partner_attribute.x_Visiting
             else:
                 res['value']['partner_caste'] = ''
                 res['value']['partner_is_tribe']= ''
                 res['value']['partner_is_sangam']= ''
                 res['value']['partner_is_Premium']= ''
+                res['value']['partner_visting']= ''
         return res;
 
     _columns = {
         'partner_caste': fields.function(_get_partner_attribute_details, type='char', string ='Tribe'),
         'partner_is_tribe': fields.function(_get_partner_attribute_Tribe_details, type='char', string ='Is Tribe'),
         'partner_is_sangam':fields.function(_get_partner_attribute_Sangam_details, type='char', string ='Is Sangam'),
-        'partner_is_Premium':fields.function(_get_partner_attribute_Sangam_details, type='char', string ='Is Premium',),
+        'partner_is_Premium':fields.function(_get_partner_attribute_Premium_details, type='char', string ='Is Premium',),
+        'partner_visting':fields.function(_get_partner_attribute_Visiting, type='char', string ='Visiting',),
     }
 sale_order()
