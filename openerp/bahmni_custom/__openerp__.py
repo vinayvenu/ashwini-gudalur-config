5ee4c7090d9b711b044a9b810816a41fe339f0a7
sandye <sandeepelambassery@gmail.com>
2016-04-12 14:32:54 +0530

    Added caresetting to saleorder tree view

--- openerp/bahmni_custom/__openerp__.py
+++ openerp/bahmni_custom/__openerp__.py
@@ -3,7 +3,7 @@
     "version":"1.0",
     "author":"ThoughtWorks Technologies Pvt. Ltd.",
     "category":"BahmniCustom",
-    "description":"Custom changes for possible environment",
+    "description":"Custom changes for gudalur environment",
     "depends": ["base","bahmni_customer_payment","account_voucher","bahmni_internal_stock_move","bahmni_pharmacy_product","bahmni_purchase_extension","bahmni_stock_batch_sale_price","bahmni_sale_discount","sale","stock","purchase","account"],
     'data':['module_misc.xml','attribute_in_sale_order.xml','price_in_stock_product_lot.xml','claim_type.xml','sale_order_type_shop_form.xml','sale_care_setting_form.xml','product_category_department.xml','chargetype_category_mapping.xml','product_schedule_h_form.xml'],
     'demo':[],
--- openmrs/apps/reports/sql/admissionvisitcount.sql
+++ openmrs/apps/reports/sql/admissionvisitcount.sql
@@ -8,31 +8,21 @@ where cast(v.date_created as DATE) between '#startDate#' and '#endDate#' ORDER B
          join person_attribute pa on p.patient_id=pa.person_id and pa.person_attribute_type_id=27 AND (pa.value=1 or pa.value= 2146)
          join visit v on p.patient_id = v.patient_id
          join visit_type vt on vt.visit_type_id = v.visit_type_id and vt.visit_type_id=(select visit_type_id from visit_type where name='OPD')
-         join encounter e on e.visit_id=v.visit_id
        where cast(v.date_created as DATE)between '#startDate#' and '#endDate#' ORDER BY v.visit_id) as TribalOPD,
        (select count(person_id) from patient p
          join person_attribute pa on p.patient_id=pa.person_id and pa.person_attribute_type_id=27 AND (pa.value=1 or pa.value= 2146)
          join visit v on p.patient_id = v.patient_id
          join visit_type vt on vt.visit_type_id = v.visit_type_id and vt.visit_type_id=(select visit_type_id from visit_type where name='Emergency')
-         join encounter e on e.visit_id=v.visit_id
        where cast(v.date_created as DATE) between '#startDate#' and '#endDate#' ORDER BY v.visit_id) as TribalEmergency,
        (select count(person_id) from patient p
          join person_attribute pa on p.patient_id=pa.person_id and pa.person_attribute_type_id=27 AND (pa.value=1 or pa.value= 2146)
          join visit v on p.patient_id = v.patient_id
-         join visit_type vt on vt.visit_type_id = v.visit_type_id and vt.visit_type_id=(select visit_type_id from visit_type where name='Special OPD')
-         join encounter e on e.visit_id=v.visit_id
-       where cast(v.date_created as DATE) between '#startDate#' and '#endDate#' ORDER BY v.visit_id) as TribalSplOPD,
-       (select count(person_id) from patient p
-         join person_attribute pa on p.patient_id=pa.person_id and pa.person_attribute_type_id=27 AND (pa.value=1 or pa.value= 2146)
-         join visit v on p.patient_id = v.patient_id
          join visit_type vt on vt.visit_type_id = v.visit_type_id and vt.visit_type_id=(select visit_type_id from visit_type where name='Followup')
-         join encounter e on e.visit_id=v.visit_id
        where cast(v.date_created as DATE)  between '#startDate#' and '#endDate#' ORDER BY v.visit_id) as TribalFollowUp,
        (select count(person_id) from patient p
          join person_attribute pa on p.patient_id=pa.person_id and pa.person_attribute_type_id=27 AND (pa.value=1 or pa.value= 2146)
          join visit v on p.patient_id = v.patient_id
-         join visit_type vt on vt.visit_type_id = v.visit_type_id and vt.visit_type_id=(select visit_type_id from visit_type where name='General')
-         join encounter e on e.visit_id=v.visit_id
+         join visit_type vt on vt.visit_type_id = v.visit_type_id and vt.visit_type_id=(select visit_type_id from visit_type where name='Lab Visit')
        where cast(v.date_created as DATE) between '#startDate#' and '#endDate#' ORDER BY v.visit_id)as TribalGeneral,
        (select count(person_id) from patient p
          join person_attribute pa on p.patient_id=pa.person_id and pa.person_attribute_type_id=27 AND (pa.value=2 or pa.value= 2147)
@@ -55,18 +45,12 @@ where cast(v.date_created as DATE) between '#startDate#' and '#endDate#' ORDER B
        (select count(person_id) from patient p
          join person_attribute pa on p.patient_id=pa.person_id and pa.person_attribute_type_id=27 AND (pa.value=2 or pa.value= 2147)
          join visit v on p.patient_id = v.patient_id
-         join visit_type vt on vt.visit_type_id = v.visit_type_id and vt.visit_type_id=(select visit_type_id from visit_type where name='Special OPD')
-         join encounter e on e.visit_id=v.visit_id
-       where cast(v.date_created as DATE) between '#startDate#' and '#endDate#' ORDER BY v.visit_id) as NonTribalSplOPD,
-       (select count(person_id) from patient p
-         join person_attribute pa on p.patient_id=pa.person_id and pa.person_attribute_type_id=27 AND (pa.value=2 or pa.value= 2147)
-         join visit v on p.patient_id = v.patient_id
          join visit_type vt on vt.visit_type_id = v.visit_type_id and vt.visit_type_id=(select visit_type_id from visit_type where name='Followup')
          join encounter e on e.visit_id=v.visit_id
        where cast(v.date_created as DATE) between '#startDate#' and '#endDate#' ORDER BY v.visit_id) as NonTribalFollowUp,
        (select count(person_id) from patient p
          join person_attribute pa on p.patient_id=pa.person_id and pa.person_attribute_type_id=27 AND (pa.value=2 or pa.value= 2147)
          join visit v on p.patient_id = v.patient_id
-         join visit_type vt on vt.visit_type_id = v.visit_type_id and vt.visit_type_id=(select visit_type_id from visit_type where name='General')
+         join visit_type vt on vt.visit_type_id = v.visit_type_id and vt.visit_type_id=(select visit_type_id from visit_type where name='Lab Visit')
          join encounter e on e.visit_id=v.visit_id
        where cast(v.date_created as DATE) between '#startDate#' and '#endDate#' ORDER BY v.visit_id)as NonTribalGeneral;
